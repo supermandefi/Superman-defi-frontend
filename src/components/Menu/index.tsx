6a2bb8c2d9eb40355117a265ec6b7f473ad3de51
bababobo09 <81358335+bababobo09@users.noreply.github.com>
2021-03-26 20:37:50 +0100

    Update index.tsx

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -25,7 +25,7 @@ const Menu = (props) => {
       setLang={setSelectedLanguage}
       cakePriceUsd={cakePriceUsd.toNumber()}
       links={config}
-      priceLink="https://pancakeswap.info/token/0x440676a18d9792215500b1bc06341eb169e6e74b"
+      priceLink="https://pancakeswap.info/token/0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
       {...props}
     />
   )
